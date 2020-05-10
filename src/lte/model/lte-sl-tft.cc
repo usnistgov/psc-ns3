b3693303fb4fa6eb5e512f09e64004f5b10b7ba9
ZorazeAli <zoraze.ali@cttc.es>
2020-05-10 15:21:56 +0200

    lte: Add sidelink traffic flow template class API

---
+++ src/lte/model/lte-sl-tft.cc
@@ -0,0 +1,153 @@
+/* -*-  Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * NIST-developed software is provided by NIST as a public
+ * service. You may use, copy and distribute copies of the software in
+ * any medium, provided that you keep intact this entire notice. You
+ * may improve, modify and create derivative works of the software or
+ * any portion of the software, and you may copy and distribute such
+ * modifications or works. Modified works should carry a notice
+ * stating that you changed the software and should note the date and
+ * nature of any such change. Please explicitly acknowledge the
+ * National Institute of Standards and Technology as the source of the
+ * software.
+ *
+ * NIST-developed software is expressly provided "AS IS." NIST MAKES
+ * NO WARRANTY OF ANY KIND, EXPRESS, IMPLIED, IN FACT OR ARISING BY
+ * OPERATION OF LAW, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE,
+ * NON-INFRINGEMENT AND DATA ACCURACY. NIST NEITHER REPRESENTS NOR
+ * WARRANTS THAT THE OPERATION OF THE SOFTWARE WILL BE UNINTERRUPTED
+ * OR ERROR-FREE, OR THAT ANY DEFECTS WILL BE CORRECTED. NIST DOES NOT
+ * WARRANT OR MAKE ANY REPRESENTATIONS REGARDING THE USE OF THE
+ * SOFTWARE OR THE RESULTS THEREOF, INCLUDING BUT NOT LIMITED TO THE
+ * CORRECTNESS, ACCURACY, RELIABILITY, OR USEFULNESS OF THE SOFTWARE.
+ *
+ * You are solely responsible for determining the appropriateness of
+ * using and distributing the software and you assume all risks
+ * associated with its use, including but not limited to the risks and
+ * costs of program errors, compliance with applicable laws, damage to
+ * or loss of data, programs or equipment, and the unavailability or
+ * interruption of operation. This software is not intended to be used
+ * in any situation where a failure could cause risk of injury or
+ * damage to property. The software developed by NIST employees is not
+ * subject to copyright protection within the United States.
+ */
+
+#include "lte-sl-tft.h"
+#include "ns3/abort.h"
+#include "ns3/log.h"
+
+namespace ns3 {
+
+NS_LOG_COMPONENT_DEFINE ("LteSlTft");
+
+LteSlTft::LteSlTft (Direction d, CommType commType, Ipv4Address remoteAddr, uint32_t remoteL2Id)
+{
+  NS_LOG_FUNCTION (this);
+  m_direction = d;
+  m_commType = commType;
+  m_hasRemoteAddress = true;
+  m_remoteAddress = remoteAddr;
+  m_remoteMask = Ipv4Mask::GetOnes ();
+
+  NS_ASSERT_MSG (remoteL2Id > 0, "Remote L2 id must be greater than zero");
+  NS_ASSERT_MSG ((remoteL2Id & 0xFF000000) == 0, "Remote L2 id must be 24 bits");
+  m_remoteL2Id = remoteL2Id;
+}
+
+LteSlTft::LteSlTft (Direction d, CommType commType, Ipv6Address remoteAddr, uint32_t remoteL2Id)
+{
+  NS_LOG_FUNCTION (this);
+  m_direction = d;
+  m_commType = commType;
+  m_hasRemoteAddress = true;
+  m_remoteAddress6 = remoteAddr;
+  m_remoteMask6 = Ipv6Prefix::GetOnes ();
+
+  NS_ASSERT_MSG (remoteL2Id > 0, "Remote L2 id must be greater than zero");
+  NS_ASSERT_MSG ((remoteL2Id & 0xFF000000) == 0, "Remote L2 id must be 24 bits");
+  m_remoteL2Id = remoteL2Id;
+}
+
+LteSlTft::LteSlTft (Ptr<LteSlTft> tft)
+{
+  NS_LOG_FUNCTION (this);
+
+  m_direction = tft->m_direction;
+  m_commType = tft->m_commType;
+  m_remoteAddress = tft->m_remoteAddress;
+  m_remoteAddress6 = tft->m_remoteAddress6;
+  m_remoteMask = tft->m_remoteMask;
+  m_remoteMask6 = tft->m_remoteMask6;
+  m_remoteL2Id = tft->m_remoteL2Id;
+}
+
+bool
+LteSlTft::Matches (Ipv4Address ra)
+{
+  NS_LOG_FUNCTION (this << ra);
+  bool ok = false;
+  //check remote address
+  ok = m_hasRemoteAddress ? m_remoteMask.IsMatch (ra, m_remoteAddress) : true;
+  return ok;
+}
+
+bool
+LteSlTft::Matches (Ipv6Address ra)
+{
+  NS_LOG_FUNCTION (this << ra);
+  bool ok = false;
+  //check remote address
+  ok = m_hasRemoteAddress ? m_remoteMask6.IsMatch (ra, m_remoteAddress6) : true;
+  return ok;
+}
+
+bool
+LteSlTft::Equals (Ptr<LteSlTft> tft)
+{
+  NS_LOG_FUNCTION (this);
+  bool equals = true;
+
+  if (m_direction != tft->m_direction
+      && m_remoteAddress != tft->m_remoteAddress
+      && m_remoteAddress6 != tft->m_remoteAddress6
+      && m_remoteMask != tft->m_remoteMask
+      && m_remoteMask6 != tft->m_remoteMask6
+      && m_remoteL2Id != tft->m_remoteL2Id)
+    {
+      equals = false;
+    }
+
+  return equals;
+}
+
+uint32_t
+LteSlTft::GetRemoteL2Id()
+{
+  return m_remoteL2Id;
+}
+
+bool
+LteSlTft::isReceive ()
+{
+  //receiving if RECEIVE or BIDIRECTIONAL
+  NS_ASSERT_MSG (m_direction != LteSlTft::Direction::INVALID, "Invalid TFT direction");
+  return m_direction != LteSlTft::Direction::TRANSMIT;
+}
+
+bool
+LteSlTft::isTransmit ()
+{
+  //transmitting if TRANSMIT or BIDIRECTIONAL
+  NS_ASSERT_MSG (m_direction != LteSlTft::Direction::INVALID, "Invalid TFT direction");
+  return m_direction != LteSlTft::Direction::RECEIVE;
+}
+
+bool
+LteSlTft::isUnicast ()
+{
+  NS_ASSERT_MSG (m_commType != LteSlTft::CommType::INVALID, "Invalid TFT communication type");
+  return (m_commType != LteSlTft::CommType::Broadcast && m_commType != LteSlTft::CommType::GroupCast);
+}
+
+} // namespace ns3
---
+++ src/lte/model/lte-sl-tft.h
@@ -0,0 +1,176 @@
+/* -*-  Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * NIST-developed software is provided by NIST as a public
+ * service. You may use, copy and distribute copies of the software in
+ * any medium, provided that you keep intact this entire notice. You
+ * may improve, modify and create derivative works of the software or
+ * any portion of the software, and you may copy and distribute such
+ * modifications or works. Modified works should carry a notice
+ * stating that you changed the software and should note the date and
+ * nature of any such change. Please explicitly acknowledge the
+ * National Institute of Standards and Technology as the source of the
+ * software.
+ *
+ * NIST-developed software is expressly provided "AS IS." NIST MAKES
+ * NO WARRANTY OF ANY KIND, EXPRESS, IMPLIED, IN FACT OR ARISING BY
+ * OPERATION OF LAW, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE,
+ * NON-INFRINGEMENT AND DATA ACCURACY. NIST NEITHER REPRESENTS NOR
+ * WARRANTS THAT THE OPERATION OF THE SOFTWARE WILL BE UNINTERRUPTED
+ * OR ERROR-FREE, OR THAT ANY DEFECTS WILL BE CORRECTED. NIST DOES NOT
+ * WARRANT OR MAKE ANY REPRESENTATIONS REGARDING THE USE OF THE
+ * SOFTWARE OR THE RESULTS THEREOF, INCLUDING BUT NOT LIMITED TO THE
+ * CORRECTNESS, ACCURACY, RELIABILITY, OR USEFULNESS OF THE SOFTWARE.
+ *
+ * You are solely responsible for determining the appropriateness of
+ * using and distributing the software and you assume all risks
+ * associated with its use, including but not limited to the risks and
+ * costs of program errors, compliance with applicable laws, damage to
+ * or loss of data, programs or equipment, and the unavailability or
+ * interruption of operation. This software is not intended to be used
+ * in any situation where a failure could cause risk of injury or
+ * damage to property. The software developed by NIST employees is not
+ * subject to copyright protection within the United States.
+ */
+
+#ifndef LTE_SL_TFT_H
+#define LTE_SL_TFT_H
+
+
+#include <ns3/simple-ref-count.h>
+#include <ns3/ipv4-address.h>
+#include <ns3/ipv6-address.h>
+
+#include <list>
+
+namespace ns3 {
+
+/**
+ * \ingroup Lte
+ *
+ * \brief  Traffic flow template used by sidelink bearers.
+ *
+ * This class implements a variant of LTE TFT for sidelink
+ *
+ * It is the simple version of NIST LteSlTft class in D2D repo.
+ * It uses only remote address.
+ *
+ */
+class LteSlTft : public SimpleRefCount<LteSlTft>
+{
+public:
+  /**
+   * \brief Indicates the direction of the traffic that is to be classified.
+   */
+  enum class Direction
+  {
+    TRANSMIT = 1,
+    RECEIVE = 2,
+    BIDIRECTIONAL = 3,
+    INVALID = 4
+  };
+
+  /**
+   * \brief Indicates the type of communication.
+   */
+  enum class CommType
+  {
+    Broadcast = 1,
+    GroupCast = 2,
+    Uincast = 3,
+    INVALID = 4
+  };
+
+  /**
+   * \brief Constructor (sets remote address only)
+   *
+   * \param d The direction
+   * \param remoteAddr The IPv4 address of the remote
+   * \param remoteL2Id The remote layer 2 id
+   */
+  LteSlTft (Direction d, CommType commType, Ipv4Address remoteAddr, uint32_t remoteL2Id);
+
+  /**
+   * \brief Constructor (sets remote address only)
+   *
+   * \param d The direction
+   * \param remoteAddr The IPv6 address of the remote
+   * \param remoteL2Id The remote layer 2 id
+   */
+  LteSlTft (Direction d, CommType commType, Ipv6Address remoteAddr, uint32_t remoteL2Id);
+
+  /**
+   * \brief Constructor for copy
+   *
+   * \param tft The TFT to copy
+   */
+  LteSlTft (Ptr<LteSlTft> tft);
+
+  /**
+   * \brief Function to evaluate if the SL TFT matches the remote IPv4 address
+   *
+   * \param ra the remote address
+   * \return true if the TFT matches with the parameters, false otherwise.
+   */
+  bool Matches (Ipv4Address ra);
+
+  /**
+   * \brief Function to evaluate if the SL TFT matches the remote IPv6 address
+   *
+   * \param ra the remote address
+   *
+   * \return true if the TFT matches with the
+   * parameters, false otherwise.
+   */
+  bool Matches (Ipv6Address ra);
+
+  /**
+   * \brief Function to evaluate if the SL TFT is completely equal to another SL TFT
+   *
+   * \param tft the tft to compare
+   * \return true if the provided SL TFT matches with the
+   *         actual SL TFT parameters, false otherwise.
+   */
+  bool Equals (Ptr<LteSlTft> tft);
+
+  /**
+   * \brief Gets the remote L2 id associated with the TFT
+   * \return the remote L2 address associated with the TFT
+   */
+  uint32_t GetRemoteL2Id ();
+
+  /**
+   * \brief Indicates if the TFT is for an incoming sidelink bearer
+   * \return true if the TFT is for an incoming sidelink bearer
+   */
+  bool isReceive ();
+
+  /**
+   * \brief Indicates if the TFT is for an outgoing sidelink bearer
+   * \return true if the TFT is for an outgoing sidelink bearer
+   */
+  bool isTransmit ();
+
+  /**
+   * \brief Indicates if the TFT is for unicast communication
+   * \return true if the TFT is for the unicast communication
+   */
+  bool isUnicast ();
+
+private:
+  Direction m_direction {Direction::INVALID}; /**< whether the filter needs to be applied
+                                               * to sending or receiving only, or in both cases
+                                               */
+  CommType m_commType {CommType::INVALID}; //!< The type of communication
+  bool m_hasRemoteAddress {false};      //!< Indicates if the TFT has remoteAddress information
+  Ipv4Address m_remoteAddress {Ipv4Address::GetZero ()};        //!< IPv4 address of the remote host
+  Ipv6Address m_remoteAddress6 {Ipv6Address::GetZero ()};       //!< IPv6 address of the remote host
+  Ipv4Mask m_remoteMask {Ipv4Mask::GetZero ()};         //!< IPv4 address mask of the remote host
+  Ipv6Prefix m_remoteMask6 {Ipv6Prefix::GetZero ()};    //!< IPv6 address mask of the remote host
+  uint32_t m_remoteL2Id {0};      //!< 24 bit L2 id of remote entity
+
+};
+
+} // namespace ns3
+
+#endif /* LTE_SL_TFT_H */
