6b97377cd9257ecfc6715451490e269ea64dab81
ZorazeAli <zoraze.ali@cttc.es>
2020-05-12 13:06:08 +0200

    lte: Add NrSlCommResourcePool Class

---
+++ src/lte/model/nr-sl-comm-resource-pool.cc
@@ -0,0 +1,155 @@
+/* -*-  Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil; -*- */
+/*
+ *   Copyright (c) 2019 Centre Tecnologic de Telecomunicacions de Catalunya (CTTC)
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License version 2 as
+ *   published by the Free Software Foundation;
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#include "nr-sl-comm-resource-pool.h"
+#include "ns3/log.h"
+
+namespace ns3 {
+
+NS_LOG_COMPONENT_DEFINE ("NrSlCommResourcePool");
+NS_OBJECT_ENSURE_REGISTERED (NrSlCommResourcePool);
+
+TypeId
+NrSlCommResourcePool::GetTypeId ()
+{
+  static TypeId tid = TypeId ("ns3::NrSlCommResourcePool")
+    .SetParent<Object> ()
+    .SetGroupName("lte")
+    .AddConstructor<NrSlCommResourcePool> ()
+    ;
+  return tid;
+}
+
+NrSlCommResourcePool::NrSlCommResourcePool ()
+{
+  NS_LOG_FUNCTION (this);
+}
+NrSlCommResourcePool::~NrSlCommResourcePool ()
+{
+  NS_LOG_FUNCTION (this);
+}
+
+void
+NrSlCommResourcePool::SetSlPreConfigFreqInfoList (const std::array <LteRrcSap::SlFreqConfigCommonNr, MAX_NUM_OF_FREQ_SL> &slPreconfigFreqInfoList)
+{
+  NS_LOG_FUNCTION (this);
+  m_slPreconfigFreqInfoList = slPreconfigFreqInfoList;
+}
+
+void
+NrSlCommResourcePool::SetPhysicalSlPoolMap (NrSlCommResourcePool::PhySlPoolMap phySlPoolMap)
+{
+  NS_LOG_FUNCTION (this);
+  m_phySlPoolMap = phySlPoolMap;
+}
+
+const std::vector <std::bitset<1>>
+NrSlCommResourcePool::GetPhySlPool (uint16_t bwpId, uint16_t poolId)
+{
+  NS_LOG_FUNCTION (this);
+  NrSlCommResourcePool::PhySlPoolMap::iterator itBwp = m_phySlPoolMap.find (bwpId);
+  NS_ABORT_MSG_IF (itBwp == m_phySlPoolMap.end (), "Unable to find BWP id " << bwpId);
+  std::unordered_map <uint16_t, std::vector <std::bitset<1>>>::iterator itPool = itBwp->second.find (poolId);
+  NS_ABORT_MSG_IF (itPool == itBwp->second.end (), "Unable to find pool id " << poolId);
+  return itPool->second;
+}
+
+const LteRrcSap::SlResourcePoolNr
+NrSlCommResourcePool::GetSlResourcePoolNr (uint16_t bwpId, uint16_t poolId)
+{
+  NS_LOG_FUNCTION (this);
+  LteRrcSap::SlFreqConfigCommonNr slfreqConfigCommon = m_slPreconfigFreqInfoList.at (0);
+  LteRrcSap::SlBwpConfigCommonNr slBwpConfigCommon = slfreqConfigCommon.slBwpList.at (bwpId);
+  std::array <LteRrcSap::SlResourcePoolConfigNr, MAX_NUM_OF_TX_POOL> slTxPoolSelectedNormal = slBwpConfigCommon.slBwpPoolConfigCommonNr.slTxPoolSelectedNormal;
+  LteRrcSap::SlResourcePoolNr pool;
+  bool found = false;
+  for (const auto& it: slTxPoolSelectedNormal)
+    {
+      if (it.slResourcePoolId.id == poolId)
+        {
+          found = true;
+          pool = it.slResourcePool;
+          break;
+        }
+    }
+  NS_ASSERT_MSG (found == true, "unable to find pool id " << poolId);
+  return pool;
+}
+
+std::unordered_map <uint16_t, std::vector <std::bitset<1>>>::const_iterator
+NrSlCommResourcePool::GetIteratorToPhySlPool (uint16_t bwpId, uint16_t poolId)
+{
+  NS_LOG_FUNCTION (this);
+  NrSlCommResourcePool::PhySlPoolMap::iterator itBwp = m_phySlPoolMap.find (bwpId);
+  NS_ABORT_MSG_IF (itBwp == m_phySlPoolMap.end (), "Unable to find bandwidth part id " << bwpId);
+  std::unordered_map <uint16_t, std::vector <std::bitset<1>>>::const_iterator itPool = itBwp->second.find (poolId);
+  NS_ABORT_MSG_IF (itPool == itBwp->second.end (), "Unable to find pool id " << poolId);
+  return itPool;
+}
+
+std::list <NrSlCommResourcePool::SlotInfo>
+NrSlCommResourcePool::GetSlCommOpportunities (uint16_t absIndexCurretSlot, uint16_t bwpId, uint16_t poolId, uint16_t t1)
+{
+  NS_LOG_FUNCTION (this);
+  std::unordered_map <uint16_t, std::vector <std::bitset<1>>>::const_iterator itPhyPool;
+  itPhyPool = GetIteratorToPhySlPool (bwpId, poolId);
+  LteRrcSap::SlFreqConfigCommonNr slfreqConfigCommon = m_slPreconfigFreqInfoList.at (0);
+  LteRrcSap::SlBwpConfigCommonNr slBwpConfigCommon = slfreqConfigCommon.slBwpList.at (bwpId);
+  uint16_t totalSlSymbols = LteRrcSap::GetSlLengthSymbolsValue (slBwpConfigCommon.slBwpGeneric.slLengthSymbols);
+  uint16_t slSymbolStart =  LteRrcSap::GetSlStartSymbolValue(slBwpConfigCommon.slBwpGeneric.slStartSymbol);
+
+  const LteRrcSap::SlResourcePoolNr pool = GetSlResourcePoolNr (bwpId, poolId);
+  uint16_t t2 = LteRrcSap::GetSlSelWindowValue (pool.slUeSelectedConfigRp.slSelectionWindow);
+  uint16_t firstAbsSlotIndex = absIndexCurretSlot + t1;
+  uint16_t lastAbsSlotIndex =  absIndexCurretSlot + t2;
+
+  NS_LOG_DEBUG ("Starting absolute slot number of the selection window = " << firstAbsSlotIndex);
+  NS_LOG_DEBUG ("Last absolute slot number of the selection window =  " << lastAbsSlotIndex);
+  NS_LOG_DEBUG ("Final selection Window Length = " << lastAbsSlotIndex - firstAbsSlotIndex);
+
+  std::list <NrSlCommResourcePool::SlotInfo> list;
+  uint16_t absPoolIndex = firstAbsSlotIndex % itPhyPool->second.size ();
+  NS_LOG_DEBUG ("Absolute pool index = " << absPoolIndex);
+  for (uint16_t i = firstAbsSlotIndex; i < lastAbsSlotIndex; ++i)
+    {
+
+      if (itPhyPool->second [absPoolIndex] == 1)
+        {
+          NrSlCommResourcePool::SlotInfo info;
+          //PSCCH
+          info.numSlPscchRbs = LteRrcSap::GetSlFResoPscchValue (pool.slPscchConfig.slFreqResourcePscch);
+          info.slPscchSymStart = slSymbolStart;
+          info.slPscchSymlength = LteRrcSap::GetSlTResoPscchValue (pool.slPscchConfig.slTimeResourcePscch);
+          //PSSCH
+          info.slSubchannelSize = LteRrcSap::GetSlSubChSizeValue (pool.slSubchannelSize);
+          info.slPsschSymStart = info.slPscchSymStart + info.slPscchSymlength;
+          info.slPsschSymlength = (totalSlSymbols - info.slPscchSymlength) - 1;
+          info.absSlotIndex = i;
+          list.emplace_back (info);
+        }
+      ++absPoolIndex;
+    }
+
+  return list;
+}
+
+
+}
+
+
---
+++ src/lte/model/nr-sl-comm-resource-pool.h
@@ -0,0 +1,145 @@
+/* -*-  Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil; -*- */
+/*
+ *   Copyright (c) 2019 Centre Tecnologic de Telecomunicacions de Catalunya (CTTC)
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License version 2 as
+ *   published by the Free Software Foundation;
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#ifndef NR_SL_COMM_RESOURCE_POOL
+#define NR_SL_COMM_RESOURCE_POOL
+
+#include <ns3/object.h>
+#include <ns3/lte-rrc-sap.h>
+
+#include <stdint.h>
+#include <list>
+#include <array>
+#include <bitset>
+#include <unordered_map>
+
+namespace ns3 {
+
+
+class NrSlCommResourcePool : public Object
+{
+
+public:
+  struct SlotInfo
+  {
+    //PSCCH
+    uint16_t numSlPscchRbs {0};
+    uint16_t slPscchSymStart {0};
+    uint16_t slPscchSymlength {0};
+    //PSSCH
+    uint16_t slPsschSymStart {0};
+    uint16_t slPsschSymlength {0};
+    //subchannel size in RBs
+    uint16_t slSubchannelSize {0};
+    //Absolute slot number
+    uint16_t absSlotIndex {0};
+  };
+  /**
+   * \brief Map to store the physical SL pool per BWP and per SL pool
+   *
+   * Key of the first map is the BWP id
+   * Value of the first map is an unordered map storing physical SL pool per SL pool
+   * Key of the second map is the pool id
+   * Value of the second map is a vector, which is a physical SL pool
+   */
+  typedef std::unordered_map<uint16_t, std::unordered_map <uint16_t, std::vector <std::bitset<1>>> > PhySlPoolMap;
+
+  /**
+   * \brief Constructor
+   */
+  NrSlCommResourcePool ();
+  /**
+   * \brief Destructor
+   */
+  virtual ~NrSlCommResourcePool ();
+
+  /**
+   * \brief Get the type ID.
+   * \return the object TypeId
+   */
+  static TypeId GetTypeId ();
+
+  /**
+   * \brief Set sidelink preconfiguration  frequency information list
+   *
+   * \param slPreconfigFreqInfoList A list containing per carrier configuration for NR sidelink communication
+   */
+  void SetSlPreConfigFreqInfoList (const std::array <LteRrcSap::SlFreqConfigCommonNr, MAX_NUM_OF_FREQ_SL> &slPreconfigFreqInfoList);
+  /**
+   * \brief Set physical sidelink pool map
+   *
+   * \param phySlPoolMap The Map containing the physical SL pool per BWP and per SL pool
+   */
+  void SetPhysicalSlPoolMap (NrSlCommResourcePool::PhySlPoolMap phySlPoolMap);
+  /**
+   * \brief Get physical sidelink pool
+   *
+   * \param bwpId The bandwidth part id
+   * \param poolId The pool id
+   * \return A vector representing the the physical sidelink pool
+   */
+  const std::vector <std::bitset<1>> GetPhySlPool (uint16_t bwpId, uint16_t poolId);
+  /**
+   * \brief Get sidelink communication opportunities
+   *
+   * TS 38.214 8.1.2.1 <b>"Within the slot, PSSCH resource allocation starts at symbol startSLsymbols+1"</b>
+   * Since we are multiplexing the PSCCH and PSSCH in time, we are already
+   * taking the TS 38.214 (8.1.2.1) into the consideration. That is,
+   * <b>1.</b> \f$number of PSSCH symbols = Total symbols - number of PSCCH symbols\f$
+   * TS 38.214 8.1.2.1 also says <b>"The UE shall not transmit PSSCH in the last symbol configured for sidelink.</b>
+   * Therefore, we to subtract one more symbol from the PSSCH symbols, i.e.,
+   * <b>2.</b> \f$Total number of PSSCH symbols = number of PSSCH symbols (from 1) - 1\f$
+   *
+   * \param numAbsSlot The current absolute slot number
+   * \param bwpId The bandwidth part id
+   * \param poolId The pool id
+   * \param t1 The physical layer processing delay in slots
+   * \return A list of sidelink communication opportunities for each available slot in a selection window
+   */
+  std::list <NrSlCommResourcePool::SlotInfo> GetSlCommOpportunities (uint16_t numAbsSlot, uint16_t bwpId, uint16_t poolId, uint16_t t1);
+
+
+
+private:
+  /**
+   * \brief Get iterator to physical sidelink pool
+   *
+   * \param bwpId The bandwidth part id
+   * \param poolId The pool id
+   * \return An iterator to the stored physical sidelink pool
+   */
+  std::unordered_map <uint16_t, std::vector <std::bitset<1>>>::const_iterator GetIteratorToPhySlPool (uint16_t bwpId, uint16_t pooId);
+  /*
+   * \brief Get SlResourcePoolNr
+   *
+   * \param bwpId The BWP id
+   * \param pooI The pool id
+   * \return The LteRrcSap::SlResourcePoolNr, which holds the SL pool related configuration
+   */
+  const LteRrcSap::SlResourcePoolNr GetSlResourcePoolNr (uint16_t bwpId, uint16_t poolId);
+  std::array <LteRrcSap::SlFreqConfigCommonNr, MAX_NUM_OF_FREQ_SL> m_slPreconfigFreqInfoList; //!< A list containing per carrier configuration for NR sidelink communication
+  PhySlPoolMap m_phySlPoolMap; //!< A map to store the physical SL pool per BWP and per SL pool
+
+};
+
+
+
+}
+
+#endif /* NR_SL_COMM_RESOURCE_POOL */
